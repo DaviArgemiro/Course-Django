9fc9ba1bb76d61dd54c13cf7026a880c53045a70
Davi <davifilho.br@gmail.com>
2024-09-19 15:36:33 -0300

    Principal past of the course of Django

---
+++ Django/Carros/cars/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from cars.models import Car, Brand
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model',)
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
