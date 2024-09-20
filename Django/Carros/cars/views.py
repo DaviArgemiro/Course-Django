fedeb572c6994a98a5be389aa6470c94038b070e
Davi <davifilho.br@gmail.com>
2024-09-20 18:16:23 -0300

    Learning about templates in django

--- Django/Carros/cars/views.py
+++ Django/Carros/cars/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def cars_view(request):
-    return HttpResponse('Meus carros')
+    return render(request, 'cars.html')
