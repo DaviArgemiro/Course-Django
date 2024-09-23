b3b9fe4551060b63f2da34452f712b47abd40edc
Davi <davifilho.br@gmail.com>
2024-09-23 12:01:37 -0300

    Learning about ORM functions in Django

--- Django/Carros/cars/views.py
+++ Django/Carros/cars/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    return render(request, 'cars.html')
+    cars = Car.objects.filter(brand__name='Fiat')
+
+    return render(
+            request,
+            'cars.html', 
+            {'cars': cars}
+        )
