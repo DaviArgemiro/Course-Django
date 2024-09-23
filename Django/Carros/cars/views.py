0d709fee9ce5264828aefdcd7e307abfe77ff6e1
Davi <davifilho.br@gmail.com>
2024-09-23 16:25:01 -0300

    Learning more about ORM and Django templates

--- Django/Carros/cars/views.py
+++ Django/Carros/cars/views.py
@@ -3,7 +3,12 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    cars = Car.objects.filter(brand__name='Fiat')
+    cars = Car.objects.all().order_by('brand__name')
+    search = request.GET.get('search')
+
+    if(search):
+        cars = Car.objects.filter(model__contains=search)
+    
 
     return render(
             request,
